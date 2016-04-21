836d7b9f3ba8bdf440d86a9de8555c8a284aa9fd
冷一萧6 <464596299@qq.com>
2016-04-21 22:48:23 +0800

    Initial Commit

---
+++ FXImageScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FXImageScroll
+//
+//  Created by Benniu15 on 16/4/21.
+//  Copyright © 2016年 Wind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FXImageScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FXImageScroll
+//
+//  Created by Benniu15 on 16/4/21.
+//  Copyright © 2016年 Wind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
